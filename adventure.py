a07384e0eddb10f3a9a1502c486ec41c5005d70b
blobenterprises <blobenterprises@hotmail.com>
2018-03-09 21:48:12 +0000

    First commit

---
+++ adventure.py
@@ -0,0 +1,9 @@
+print ("Welcome to Adventure v1.0.1")
+name = input("What is your name? ")
+print ("You are in a forest.")
+print ("You see a dwarf.")
+print ("The dwarf says \"Fuck you, "+name+"!\"")
+if(name == "Alex"):
+    print ("The dwarf fucks you up.")
+else: print ("The dwarf hits you.")
+print ("You die...")
